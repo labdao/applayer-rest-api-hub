a598fd5167946647029a6ff8e07edf1cf06ee712
Rik Smith-Unna <rik@fathomlabs.io>
2022-02-11 04:13:43 +0000

    handle unknown jobId

--- src/apps/alphafold/api.ts
+++ src/apps/alphafold/api.ts
@@ -7,6 +7,17 @@ export const submit = (_req: any, res: any) => {
   return res.send(docsData.endpoints['/alphafold/submit'].response_example)
 }
 
-export const status = (_req: any, res: any) => {
+export const status = (req: any, res: any) => {
+  console.log(req)
+  const { jobId } = req.params
+
+  if (!jobId) {
+    return res.send({ message: "error: jobId not provided" }, 400)
+  }
+
+  if (jobId !== '12345') {
+    return res.send({ message: "error: jobId does not exist" }, 400)
+  }
+
   return res.send(docsData.endpoints['/alphafold/status/:jobId'].response_example)
 }
